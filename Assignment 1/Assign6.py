c23bbf87ea1dc51dd977f56e3efba3fab6fc8dbf
Dibyataru Chakraborty <64766478+Dib2001@users.noreply.github.com>
2022-08-01 21:05:42 +0530

    Update Assign6.py

--- Assignment 1/Assign6.py
+++ Assignment 1/Assign6.py
@@ -6,4 +6,4 @@ l2=int(input("Enter the 2nd length: "))
 l3=int(input("Enter the 3rd length: "))
 s=(l1+l2+l3)/2
 a=math.sqrt(s*(s-l1)*(s-l2)*(s-l3))
-print("The area of triangle: ",a)
+print("The area of triangle:%.2f "%a)
