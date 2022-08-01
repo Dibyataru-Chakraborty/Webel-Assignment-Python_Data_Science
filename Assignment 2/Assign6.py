23240de9f80cddfaa71c9d946cfceec72ad7ef27
Dibyataru Chakraborty <64766478+Dib2001@users.noreply.github.com>
2022-08-01 21:18:55 +0530

    Update Assign6.py

--- Assignment 2/Assign6.py
+++ Assignment 2/Assign6.py
@@ -1,11 +1,9 @@
 #Write a python script to take month value in numeric format and display number of days in it
 month = int(input())
-year = int(input())
 if(month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
 	print("Number of days is 31")
-elif(month == 2 and (year%400==0 or year%4==0 and year%100!=0)):
 	print("Number of days is 29")
 elif(month == 2):
-	print("Number of days is 28")
+	print("Number of days is 28 or 29")
 else:
 	print("Number of days is 30")
