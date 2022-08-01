e4d1d79d3278924ec819b9b47cfd4f645c5b1f49
Dibyataru Chakraborty <64766478+Dib2001@users.noreply.github.com>
2022-08-01 20:57:28 +0530

    Update Assign3.py

--- Assignment 1/Assign3.py
+++ Assignment 1/Assign3.py
@@ -4,4 +4,4 @@ l=int(input("Enter the length: "))
 h=int(input("Enter the height: "))
 w=int(input("Enter the width: "))
 v=l*h*w
-print("The volume of circle: ",v)
+print("The volume of cuboid: ",v)
