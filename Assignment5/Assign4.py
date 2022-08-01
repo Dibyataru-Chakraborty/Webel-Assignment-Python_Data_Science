7b5ac9d256ed1aa8f73837e0f060882e0eb3bbde
Dibyataru Chakraborty <64766478+Dib2001@users.noreply.github.com>
2022-08-01 22:08:42 +0530

    Update Assign4.py

--- Assignment5/Assign4.py
+++ Assignment5/Assign4.py
@@ -3,4 +3,4 @@ a=int(input("Enter 1st number: "))
 b=int(input("Enter 2nd number: "))
 for i in range(a,b+1):
     sq=i**2
-    print("The square of {}: {}".format(i,sq))
+    print("The square of {0}: {1}".format(i,sq))
