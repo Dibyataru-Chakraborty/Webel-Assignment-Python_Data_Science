da0cd1d6aa8928039c67b1e76a3cf838b3e44894
Dibyataru Chakraborty <64766478+Dib2001@users.noreply.github.com>
2022-08-01 22:08:18 +0530

    Update Assign2.py

--- Assignment5/Assign2.py
+++ Assignment5/Assign2.py
@@ -2,5 +2,5 @@
 n=int(input("Enter the number to to print table: "))
 for i in range(1,11):
     mul=n*i
-    print("{} x {}=".format(n,i),end=" ")
+    print("{0} x {1}=".format(n,i),end=" ")
     print(mul)
